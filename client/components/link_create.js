dee4c252ffd56380f1765cd9a6d5525dd1cc92b0
eddanieles <ed.danieles@yahoo.com>
2017-02-20 08:16:18 -0500

    validating url

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -4,14 +4,14 @@ class LinkCreate extends Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    Meteor.call('links.insert', this.refs.input.value)
+    Meteor.call('links.insert', this.refs.link.value)
   }
   render() {
     return (
       <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link to shorten</label>
-          <input className="form-control" ref="input"/>
+          <input className="form-control" ref="link"/>
         </div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
