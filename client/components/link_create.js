720cf955afc6e44c1eff61d40d8819c00993fdab
eddanieles <ed.danieles@yahoo.com>
2017-02-20 07:57:47 -0500

    attempting to save url

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 
 class LinkCreate extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    Meteor.call('links.insert', this.refs.input.value)
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link to shorten</label>
-          <input className="form-control" />
+          <input className="form-control" ref="input"/>
         </div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
